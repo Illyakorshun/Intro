c68f009d67dd4dccfa484c3d7ec8dbe45c4709aa
student <korsillua@gmail.com>
2025-05-07 20:35:14 +0300

    `11`

--- Intro/obj/Debug/net9.0/Intro.AssemblyInfo.cs
+++ Intro/obj/Debug/net9.0/Intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dab63920da92afe887e8092d32c9df0675f90b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
