ec3d0aaccf0bd15c8e42e57b7c3edb8e1c5823e8
Студент <student@ITStep.local>
2025-05-07 19:46:12 +0300

    Initial commit

---
+++ Intro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Number 1 - 100: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Enter Number 1 - 100: ");
+            }
+            else
+            {
+                bool divisibleBy3 = number % 3 == 0;
+                bool divisibleBy5 = number % 5 == 0;
+
+                if (divisibleBy3 && divisibleBy5)
+                {
+                    Console.WriteLine("FIZZ BUZZ ");
+                }
+                else if (divisibleBy3)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                else if (divisibleBy5)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter Number 1 - 100: ");
+        }
+}
+}
